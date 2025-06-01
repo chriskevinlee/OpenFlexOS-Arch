e06bb40c11e958eb967226080c84af27dd241a58
chriskevinlee <leechriskevin@gmail.com>
2025-06-01 20:53:22 +0100

    Added the option to install openbox

--- modules/WM_to_Skell.sh
+++ modules/WM_to_Skell.sh
@@ -3,10 +3,13 @@ mkdir /etc/skel/.config/wallpapers
 git clone https://github.com/chriskevinlee/wallpaper_cave_nature.git /etc/skel/.config/wallpapers/wallpaper_cave_nature/
 #git clone https://github.com/chriskevinlee/OpenFlexOS-Configs.git ./OpenFlexOS-Configs
 
-while getopts 'QBO' configs; do
+while getopts 'QO' configs; do
 case $configs in
 	Q )
 		cp -r ./OpenFlexOS-Configs/config/qtile /etc/skel/.config/qtile/
+		;;
+	O )
+		cp -r ./OpenFlexOS-Configs/config/openbox /etc/skel/.config/openbox/
 		;;
 esac
 
--- modules/WindowManagers.sh
+++ modules/WindowManagers.sh
@@ -1,9 +1,12 @@
-while getopts 'QBO' configs; do
+while getopts 'QO' configs; do
 case $configs in
 	Q )
-        
 		pacman --noconfirm --needed -S qtile
 		rm /usr/share/wayland-sessions/qtile-wayland.desktop
+		;;
+	O )
+		pacman --noconfirm --needed -S openbox
+		pacman --noconfirm --needed -S tint2
 		;;
 esac
 
