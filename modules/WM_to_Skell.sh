dd11e9f54166f63aa09fcf2278d39ae0704d50b1
chriskevinlee <112503262+chriskevinlee@users.noreply.github.com>
2025-06-07 15:05:39 +0100

    Update WM_to_Skell.sh

--- modules/WM_to_Skell.sh
+++ modules/WM_to_Skell.sh
@@ -1,7 +1,6 @@
 mkdir /etc/skel/.config
 mkdir /etc/skel/.config/wallpapers
 git clone https://github.com/chriskevinlee/wallpaper_cave_nature.git /etc/skel/.config/wallpapers/wallpaper_cave_nature/
-git clone https://github.com/chriskevinlee/OpenFlexOS-Configs.git ./OpenFlexOS-Configs
 
 while getopts 'QO' configs; do
 case $configs in
