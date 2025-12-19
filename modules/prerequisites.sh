45802b305fccd5e50d9460f6e8a338084489b866
chriskevinlee <leechriskevin@gmail.com>
2025-12-19 21:35:04 +0000

    Updated prerequisites.sh for openflexos repo to point to the correct url

--- modules/prerequisites.sh
+++ modules/prerequisites.sh
@@ -15,7 +15,7 @@ case "${yn,,}" in
 
 [openflexos]
 SigLevel = Optional TrustAll
-Server = https://chriskevinlee.github.io/TestRepo-OpenFlexOS-Packages/
+Server = https://chriskevinlee.github.io/OpenFlexOS-ArchPackages/$arch
 EOF'
       echo "✅ Repository added successfully."
     fi
